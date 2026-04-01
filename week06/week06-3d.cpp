85dea4bb8936eb870f9f9493ba12b8ffdd80b257
weiwaishih <14750535@me.mcu.edu.tw>
2026-04-01 11:47:46 +0800

    week06

---
+++ week06/week06-3d.cpp
@@ -0,0 +1,10 @@
+// week06-3d.cpp SOIT06_BASE_005
+#include <stdio.h>
+int main()
+{
+	printf("Enter two numbers: ");
+	int a, b;
+	scanf("%d%d", &a, &b);
+	//printf("%d\n", a%b);
+	printf("The remainder is %d\n", a%b);
+}
